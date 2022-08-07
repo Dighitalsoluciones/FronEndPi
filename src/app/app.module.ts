f30d8b947e50780ac8749b07d4ee9b14ecbf1a55
Dighitalsoluciones <hernanglomba@gmail.com>
2022-08-07 19:20:38 -0300

    los primeros dos componentes agregados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { BarrainicialComponent } from './componentes/barrainicial/barrainicial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    BarrainicialComponent
   ],
   imports: [
     BrowserModule
