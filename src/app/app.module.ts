6b9adf88b1c3f53c0cf95126c652c2f5b6ed5b9c
Dighitalsoluciones <hernanglomba@gmail.com>
2022-08-08 14:16:46 -0300

    App modulada completa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
 import { BarrainicialComponent } from './componentes/barrainicial/barrainicial.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciasComponent } from './componentes/experiencias/experiencias.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { HysSkillsComponent } from './componentes/hys-skills/hys-skills.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EncabezadoComponent,
-    BarrainicialComponent
+    BarrainicialComponent,
+    AcercaDeComponent,
+    ExperienciasComponent,
+    EducacionComponent,
+    HysSkillsComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule
